8c0d4d42859418d0db224a58616bcaec650d56db
Clément <clement.girard1903@gmail.com>
2026-01-09 21:23:50 +0100

    add favicon

--- app.py
+++ app.py
@@ -96,5 +96,11 @@ def dashboard():
     return render_template('index.html', **fetch_data())
 
 
+# Route pour favicon
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
